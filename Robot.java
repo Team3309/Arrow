35677c801965460e7a608d5e7ec4dd020fde5de3
Matthew Krager <metoofarmer@yahoo.com>
2014-10-16 14:19:06 -0700

    Create Robot.java

---
+++ Robot.java
@@ -0,0 +1,51 @@
+package org.team3309.Arrow;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class Robot extends IterativeRobot {
+
+  //Runs when Robot is turned on
+	public void robotInit() {
+
+	}
+
+  //When first put into disabled mode
+	public void disabledInit() {
+	  
+	}
+	
+	//Called repeatedly in disabled mode
+	public void disabledPeriodic() {
+	
+	}
+	
+  //Init to Auto
+	public void autonomousInit() {
+	
+	}
+
+	
+	//This function is called periodically during autonomous
+	public void autonomousPeriodic() {
+		
+	}
+
+  //Init to Tele
+	public void teleopInit() {
+		
+	}
+
+	
+	//This function is called periodically during operator control
+	public void teleopPeriodic() {
+		
+	}
+}
