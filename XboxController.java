e0aad7e6f88c328869c68c74d35e9725b5c888d9
Matthew Krager <metoofarmer@yahoo.com>
2014-10-27 21:28:04 -0700

    Added Button Get Methods

--- XboxController.java
+++ XboxController.java
@@ -19,3 +19,67 @@ public class XboxController extends GenericHID{
   }
   
   
+  //Now, here are all the button methods, they all return a boolean that returns true if button is pressed (obviously)
+  public boolean getA() {
+    return Controller.getRawButton(XboxControllerMap.BUTTON_A);
+  } 
+  
+  public boolean getB() {
+    return Controller.getRawButton(XboxControllerMap.BUTTON_B);
+  } 
+  
+  public boolean getX() {
+    return Controller.getRawButton(XboxControllerMap.BUTTON_X);
+  } 
+  
+  public boolean getY() {
+    return Controller.getRawButton(XboxControllerMap.BUTTON_Y);
+  } 
+  
+  public boolean getDpadUp() {
+    return Controller.getRawButton(XboxControllerMap.BUTTON_DPAD_UP);
+  } 
+  
+  public boolean getDpadDown() {
+    return Controller.getRawButton(XboxControllerMap.BUTTON_DPAD_DOWN);
+  } 
+  
+  public boolean getDpadLeft() {
+    return Controller.getRawButton(XboxControllerMap.BUTTON_DPAD_LEFT);
+  } 
+  
+  public boolean getDpadRight() {
+    return Controller.getRawButton(XboxControllerMap.BUTTON_DPAD_RIGHT);
+  } 
+  
+  public boolean getBack() {
+    return Controller.getRawButton(XboxControllerMap.BUTTON_BACK);
+  }
+  
+  public boolean getStart() {
+    return Controller.getRawButton(XboxControllerMap.BUTTON_START);
+  } 
+  
+  public boolean getLeftStickButton() {
+    return Controller.getRawButton(XboxControllerMap.BUTTON_LEFT_STICK);
+  }
+  
+  public boolean getRightStickButton() {
+    return Controller.getRawButton(XboxControllerMap.BUTTON_RIGHT_STICK);
+  } 
+  
+  public boolean getLB() {
+    return Controller.getRawButton(XboxControllerMap.BUTTON_LB);
+  } 
+  
+  public boolean getRB() {
+    return Controller.getRawButton(XboxControllerMap.BUTTON_RB);
+  } 
+  
+  public boolean getHome() {
+    return Controller.getRawButton(XboxControllerMap.BUTTON_HOME);
+  }
+  
+  
+}
+  
