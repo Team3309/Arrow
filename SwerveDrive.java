2fb78bdaad7551dedd2aacdd6f19abba34e2fc79
Matthew Krager <metoofarmer@yahoo.com>
2015-08-03 21:15:06 -0700

    commit

--- SwerveDrive.java
+++ SwerveDrive.java
@@ -1,4 +1,11 @@
+import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class SwerveDrive {
 
+public class SwerveDrive extends Subsystem{
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
 }
